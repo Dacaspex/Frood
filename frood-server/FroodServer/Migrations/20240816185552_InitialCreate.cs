e6f93b9c0fe3b31a5e5efab652345b2c59447898
Casper Smits <caspersmits11@outlook.com>
2024-08-16 20:57:13 +0200

    Added initial migration

---
+++ frood-server/FroodServer/Migrations/20240816185552_InitialCreate.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FroodServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Spaces",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spaces", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Partners",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Secret = table.Column<Guid>(type: "TEXT", nullable: false),
+                    SpaceId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partners", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Partners_Spaces_SpaceId",
+                        column: x => x.SpaceId,
+                        principalTable: "Spaces",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MoodReports",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    GlobalMood = table.Column<float>(type: "REAL", nullable: false),
+                    PartnerId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoodReports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MoodReports_Partners_PartnerId",
+                        column: x => x.PartnerId,
+                        principalTable: "Partners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MoodCategories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    MoodReportId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoodCategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MoodCategories_MoodReports_MoodReportId",
+                        column: x => x.MoodReportId,
+                        principalTable: "MoodReports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MoodTopics",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Value = table.Column<int>(type: "INTEGER", nullable: false),
+                    MoodCategoryId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoodTopics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MoodTopics_MoodCategories_MoodCategoryId",
+                        column: x => x.MoodCategoryId,
+                        principalTable: "MoodCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoodCategories_MoodReportId",
+                table: "MoodCategories",
+                column: "MoodReportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoodReports_PartnerId",
+                table: "MoodReports",
+                column: "PartnerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoodTopics_MoodCategoryId",
+                table: "MoodTopics",
+                column: "MoodCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partners_SpaceId",
+                table: "Partners",
+                column: "SpaceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MoodTopics");
+
+            migrationBuilder.DropTable(
+                name: "MoodCategories");
+
+            migrationBuilder.DropTable(
+                name: "MoodReports");
+
+            migrationBuilder.DropTable(
+                name: "Partners");
+
+            migrationBuilder.DropTable(
+                name: "Spaces");
+        }
+    }
+}
